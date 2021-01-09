3707c88de04f01e7807f7d3e0f1ce579c37074c8
Trimpl <garfj2002@yandex.by>
2021-01-09 17:31:28 +0300

    first IS setup

---
+++ Controllers/IdentityController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace auth_server.Controllers
+{
+    public class IdentityController : ControllerBase
+    {
+        [Route("identity")]
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+        
+        [Route("test")]
+        [HttpGet]
+        public async Task<IActionResult> Test()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return BadRequest(disco.Error);
+            }
+            
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return BadRequest(tokenResponse.Error);
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            return Ok(tokenResponse.Json);
+        }
+    }
+}
---
+++ Infrastructure/Extensions/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualBasic;
+
+namespace auth_server.Infrastructure.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddAccountApiAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication("auth-server")
+                .AddIdentityServerAuthentication("auth-server", options =>
+                {
+                    options.Authority = "https://localhost:5001";
+                });
+            
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("auth-server-policy", builder =>
+                {
+                    builder.AddAuthenticationSchemes("auth-server");
+                    builder.RequireScope("account");
+                });
+            });
+
+            return services;
+        }
+    }
+}
